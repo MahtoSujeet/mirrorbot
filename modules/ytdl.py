582af01d2f28d6abb93e4738e5adc8b9974d8edd
sujeetgh <sujeetmehta6@gmail.com>
2021-12-16 09:18:03 +0530

    Initial commit

---
+++ modules/ytdl.py
@@ -0,0 +1,18 @@
+from pytube import YouTube
+
+def download(arg):
+	try:
+		link= arg.split("|")[0]
+		res= arg.split("|")[1].strip()
+#		print(link+"\n"+res)
+	except IndexError:
+		return "ERROR: IndexError"
+		
+	vid = YouTube(link)
+	stream= vid.streams.filter(res=res).first()
+	file= stream.download("downloads/")
+#	print(file)
+	return file
+
+# For testing
+# download("https://youtu.be/_Lp-jRnTHKQ|144")
