446b7270716374eedd55d035eab31aefb1b6e075
sujeetgh <sujeetmehta6@gmail.com>
2021-12-16 15:58:03 +0530

    fixing mirror cmd

--- modules/mirror.py
+++ modules/mirror.py
@@ -1,7 +1,7 @@
 import requests
 
 def mirror(arg):
-	args= arg.split("|")
+	args= arg.split("|h")
 	try:
 		link= args[0].strip()
 		file_name= "downloads/"+args[1].strip()
@@ -9,6 +9,7 @@ def mirror(arg):
 		return f"ERROR : {e}"
 	
 	response= requests.get(link, stream= True)
+	open(file_name, "w").close()
 	with open(file_name, "wb") as file:
 		for chunk in response.iter_content(chunk_size= 1024):
 			file.write(chunk)
